34789500ae5332c4587743ac8fd656df155de40a
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-27 15:13:38 +0100

    primo step

--- step1/js/script.js
+++ step1/js/script.js
@@ -1,3 +1,11 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
 
-alert("ciao")
+var student = {
+    "Name": "Stefano",
+    "Surname": "Franchini",
+    "Age": 25,
+}
+
+for (var key in student) {
+    console.log(key + ": " + student[key]);
+}
