30166a360ed392d8b7fffa7b5d8023fe14a16e17
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-27 14:38:22 +0100

    commit iniziale

---
+++ step#1/js/script.js
@@ -0,0 +1,1 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
---
+++ step#3/js/script.js
@@ -0,0 +1,1 @@
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
