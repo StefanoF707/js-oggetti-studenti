b9160b2dfcb99120277cf127af625597f2b9c4a4
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-27 15:26:27 +0100

    secondo step

--- step2/js/script.js
+++ step2/js/script.js
@@ -1,3 +1,32 @@
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 
-alert("ciao2")
+var students = [
+    {
+        "Name": "Bart",
+        "Surname": "Simpson",
+    },
+    {
+        "Name": "Milhouse",
+        "Surname": "Van Houten",
+    },
+    {
+        "Name": "Ralph",
+        "Surname": "Winchester",
+    },
+    {
+        "Name": "Lisa",
+        "Surname": "Simpson",
+    },
+    {
+        "Name": "Nelson",
+        "Surname": "Muntz",
+    },
+];
+
+for (var i = 0; i < students.length; i++) {
+    var studentNumber = i + 1;
+    console.log("Studente " + studentNumber);
+    for (var key in students[i]) {
+        console.log(key + ": " + students[i][key]);
+    }
+}
