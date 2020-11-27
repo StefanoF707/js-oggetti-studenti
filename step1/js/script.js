f22a3eb9a8efe130f7170ace3138c1751dd11e42
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-27 14:54:34 +0100

    fix folders

--- step1/js/script.js
+++ step1/js/script.js
@@ -1,1 +1,3 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
+
+alert("ciao")
--- step3/js/script.js
+++ step3/js/script.js
@@ -1,1 +1,3 @@
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+alert("ciao3")
